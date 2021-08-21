b7cd7669659a7b27fbf75e88125a312f02b97e92
Abrahan N <35606009+zenith110@users.noreply.github.com>
2021-08-21 17:26:42 -0400

    Update readme.py

--- readme.py
+++ readme.py
@@ -6,9 +6,10 @@ readme_intro = "Hello, my name is Abrahan Nevarez. I'm currently working on vari
 subs = ""
 for sub in sub_data:
     if(sub["on_sale"] == "True"):
-        subs = f"\nCurrently the sub(s) on sale are: ![{sub['name']} picture]({sub['image']}){sub['name']}<br/>from {sub['last_on_sale']}<br/>"
+        subs = f"\nCurrently the sub(s) on sale are: {sub['name']}<br/>from {sub['last_on_sale']}<br/>"
     else:
         continue
 final_string = readme_intro + subs
 readme = open('README.md', 'w')
 readme.write(final_string)
+        
