105e84d8b98add1d0d5360461ac0e2143e15b500
Abrahan N <35606009+zenith110@users.noreply.github.com>
2021-08-21 17:37:32 -0400

    Got rid of the break from the h1

--- readme.py
+++ readme.py
@@ -3,7 +3,7 @@ import json
 subs = requests.get("https://api.pubsub-api.dev/onsale/")
 sub_data = json.loads(subs.text)
 readme_intro = "Hello, my name is Abrahan Nevarez. I'm currently working on various projects, but my favorite is my pubsub site.<br/>"
-pubsub_project_heading = "<h1>[Pubsub api site](https://www.pubsub-api.dev/) - [Github Repo](https://github.com/zenith110/pubsub_api)</h1><br/>"
+pubsub_project_heading = "<h1>[Pubsub api site](https://www.pubsub-api.dev/) - [Github Repo](https://github.com/zenith110/pubsub_api)</h1>"
 subs = ""
 for sub in sub_data:
     if(sub["on_sale"] == "True"):
