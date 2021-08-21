58994c67887528aa43b481e31a181f56dacd1b22
Abrahan N <35606009+zenith110@users.noreply.github.com>
2021-08-21 17:33:52 -0400

    Modified the final_string

--- readme.py
+++ readme.py
@@ -3,14 +3,13 @@ import json
 subs = requests.get("https://api.pubsub-api.dev/onsale/")
 sub_data = json.loads(subs.text)
 readme_intro = "Hello, my name is Abrahan Nevarez. I'm currently working on various projects, but my favorite is my pubsub site.<br/>"
-pubsub_project_heading = "##[Pubsub api site](https://www.pubsub-api.dev/) - [Github Repo](https://github.com/zenith110/pubsub_api)"
+pubsub_project_heading = "##[Pubsub api site](https://www.pubsub-api.dev/) - [Github Repo](https://github.com/zenith110/pubsub_api)<br/>"
 subs = ""
 for sub in sub_data:
     if(sub["on_sale"] == "True"):
         subs = f"\nCurrently the sub(s) on sale are: <br/>{sub['name']}<br/>from {sub['last_on_sale']}<br/>"
     else:
         continue
-final_string = readme_intro + subs
+final_string = readme_intro + pubsub_project_heading + subs
 readme = open('README.md', 'w')
 readme.write(final_string)
-        
