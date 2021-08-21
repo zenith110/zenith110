3c63a4cf7c8665cbe5be8f8c5a3a51ff87da2108
Abrahan N <35606009+zenith110@users.noreply.github.com>
2021-08-21 17:12:05 -0400

    Update and rename pubsub_fetcher.py to readme.py

---
+++ readme.py
@@ -0,0 +1,14 @@
+import requests
+import json
+subs = requests.get("https://api.pubsub-api.dev/onsale/")
+sub_data = json.loads(subs.text)
+readme_intro = "Hello, my name is Abrahan Nevarez. I'm currently working on various projects, but my favorite is my pubsub site."
+subs = ""
+for sub in sub_data:
+    if(sub["on_sale"] == "True"):
+        subs = f"\nCurrently the sub on sale are: {sub.image}\n{sub.name} + from {sub.last_on_sale}"
+    else:
+        continue
+final_string = readme_intro + subs
+readme = open('README.md', 'w')
+readme.write(final_string)
