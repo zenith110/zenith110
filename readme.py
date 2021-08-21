5b4e982fe4a8ab3c3886aba10612e1facaee46c4
Abrahan N <35606009+zenith110@users.noreply.github.com>
2021-08-21 18:10:53 -0400

    Update readme.py

--- readme.py
+++ readme.py
@@ -3,7 +3,7 @@ import json
 subs = requests.get("https://api.pubsub-api.dev/onsale/")
 sub_data = json.loads(subs.text)
 readme_intro = "Hello, my name is Abrahan Nevarez. I'm currently working on various projects, but my favorite is my pubsub site.<br/>"
-programming_languages = f"<h1>Programming Languages</h1> ![Golang](https://github.com/zenith110/zenith110/blob/main/golang-prog-461313.ico)![Javascript](https://github.com/zenith110/zenith110/blob/main/javascript-371774.ico) ![Python](https://github.com/zenith110/zenith110/blob/main/python-452091.ico)"
+programming_languages = f"<h1>Programming Languages</h1> <div styles='display: inline-block'>![Golang](https://github.com/zenith110/zenith110/blob/main/golang_logo_icon_171073.png)![Javascript](https://github.com/zenith110/zenith110/blob/main/javascript-371774.ico) ![Python](https://github.com/zenith110/zenith110/blob/main/python-452091.ico)</div>"
 pubsub_project_heading = "<h1>[Pubsub api site](https://www.pubsub-api.dev/) - [Github Repo](https://github.com/zenith110/pubsub_api)</h1>"
 subs = ""
 for sub in sub_data:
