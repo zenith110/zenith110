4c3846457979a31f0f161b7d2d52e3ab08eda0b7
Abrahan N <35606009+zenith110@users.noreply.github.com>
2021-08-21 18:06:43 -0400

    Swapped icons for custom ones

--- readme.py
+++ readme.py
@@ -3,7 +3,7 @@ import json
 subs = requests.get("https://api.pubsub-api.dev/onsale/")
 sub_data = json.loads(subs.text)
 readme_intro = "Hello, my name is Abrahan Nevarez. I'm currently working on various projects, but my favorite is my pubsub site.<br/>"
-programming_languages = f"<h1>Programming Languages</h1> ![Golang](https://www.iconninja.com/files/386/575/648/golang-prog-icon.png)![Javascript](https://www.iconninja.com/files/824/866/459/node-javascript-js-data-icon.png) ![Python](https://www.iconninja.com/files/757/135/810/python-icon.png)"
+programming_languages = f"<h1>Programming Languages</h1> ![Golang](https://github.com/zenith110/zenith110/blob/main/golang-prog-461313.ico)![Javascript](https://github.com/zenith110/zenith110/blob/main/javascript-371774.ico) ![Python](https://github.com/zenith110/zenith110/blob/main/python-452091.ico)"
 pubsub_project_heading = "<h1>[Pubsub api site](https://www.pubsub-api.dev/) - [Github Repo](https://github.com/zenith110/pubsub_api)</h1>"
 subs = ""
 for sub in sub_data:
