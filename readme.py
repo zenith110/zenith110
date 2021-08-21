d0752c702e5a3dd9db805f10c1b7fbbdf5944a24
Abrahan N <35606009+zenith110@users.noreply.github.com>
2021-08-21 17:35:16 -0400

    Added h1 for header for pubsub

--- readme.py
+++ readme.py
@@ -3,7 +3,7 @@ import json
 subs = requests.get("https://api.pubsub-api.dev/onsale/")
 sub_data = json.loads(subs.text)
 readme_intro = "Hello, my name is Abrahan Nevarez. I'm currently working on various projects, but my favorite is my pubsub site.<br/>"
-pubsub_project_heading = "##[Pubsub api site](https://www.pubsub-api.dev/) - [Github Repo](https://github.com/zenith110/pubsub_api)<br/>"
+pubsub_project_heading = "<h1>[Pubsub api site](https://www.pubsub-api.dev/) - [Github Repo](https://github.com/zenith110/pubsub_api)</h1><br/>"
 subs = ""
 for sub in sub_data:
     if(sub["on_sale"] == "True"):
@@ -13,3 +13,4 @@ for sub in sub_data:
 final_string = readme_intro + pubsub_project_heading + subs
 readme = open('README.md', 'w')
 readme.write(final_string)
+        
